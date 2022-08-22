8deeb5b6897b139b5687ba63e0d285b4fa78e12b
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2022-08-22 03:59:43 -0700

    Cssssssssssssssss

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Container({ children }: { children: React.ReactNode }) {
-  return <div className="w-[90%] mx-auto"> {children} </div>;
+  return <div className="container px-5 mx-auto"> {children} </div>;
 }
 
 export default Container;
